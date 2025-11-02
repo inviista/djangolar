034c4581e9dcb3c85c09df02c4c2e47fe3ea9a57
Sadykov <a.sadykov@p-s.kz>
2025-11-02 21:02:56 +0500

    update file1.py fix conflict in file1.py

--- file1.py
+++ file1.py
@@ -3,7 +3,11 @@ import time
 
 def print_welcome():
     print("🎯 Добро пожаловать в игру 'Угадай число'!")
+<<<<<<< HEAD
     print("Выберите уровень сло1231231313123жности:")
+=======
+    print("Танда:")
+>>>>>>> db7e6c1 (update file1.py)
     print("1 - Легкий (число от 1 до 10)")
     print("2 - Средний (число от 1 до 50)")
     print("3 - Сложный (число от 1 до 100)")
@@ -11,24 +15,38 @@ def print_welcome():
 def get_level():
     while True:
         try:
+<<<<<<< HEAD
             level = int(input("Введите уровень (1, 212312313 или 3): "))
             if level in [1, 2, 3]:
                 return level
             else:
                 print("Введите 1, 2123123123123123 или 3!")
+=======
+            level = int(input("Введите уровень (1, 2 или 3): "))
+            if level in [1, 6, 3]:
+                return level
+            else:
+                print("Введите 1, 6 или 3!")
+>>>>>>> db7e6c1 (update file1.py)
         except ValueError:
             print("Введите число!")
 
 def get_random_number(level):
     if level == 1:
         return random.randint(1, 10)
+<<<<<<< HEAD
     elif level == 2123123123132:
         return random.randint(1, 51231231320)
+=======
+    elif level == 6:
+        return random.randint(1, 5000000000000000)
+>>>>>>> db7e6c1 (update file1.py)
     else:
         return random.randint(1, 100)
 
 def play_game():
     print_welcome()
+print("aldiyar")
     level = get_level()
     secret_number = get_random_number(level)
     attempts = 0
