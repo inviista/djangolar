4dfde627e671cc728710ec018e03ec3adb9f3fb7
Sadykov <a.sadykov@p-s.kz>
2025-11-03 16:22:44 +0500

    practice-6-customusers

--- config/settings.py
+++ config/settings.py
@@ -41,8 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalogs',
     'commerces',
-    'abstracts'
+    'abstracts',
+    'users'
 ]
+
+AUTH_USER_MODEL = "users.CustomUser"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
