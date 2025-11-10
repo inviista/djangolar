adadc76eb31e7989f62826507a35bc3fbfb7353c
Sadykov <a.sadykov@p-s.kz>
2025-11-10 12:28:55 +0500

    practice-7-orm

--- users/models.py
+++ users/models.py
@@ -1,27 +1,63 @@
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils import timezone
 
-class CustomUser(AbstractUser):
-    phone = models.CharField(max_length=20, blank=True, null=True)
+DEPARTMENTS = [
+    ("IT", "IT"),
+    ("HR", "HR"),
+    ("Sales", "Sales"),
+    ("Finance", "Finance"),
+]
 
-class AdvancedUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        user = self.model(email=email)
+ROLES = [
+    ("admin", "admin"),
+    ("manager", "manager"),
+    ("employee", "employee"),
+]
+
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, username, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None):
-        user = self.create_user(email=email, password=password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save()
-        return user
+    def create_user(self, email, username=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, username, password, **extra_fields)
 
-class AdvancedUser(AbstractBaseUser, PermissionsMixin):
+    def create_superuser(self, email, username=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self._create_user(email, username, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, blank=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    phone = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=150, blank=True)
+    country = models.CharField(max_length=150, blank=True)
+    department = models.CharField(max_length=50, choices=DEPARTMENTS, blank=True)
+    role = models.CharField(max_length=20, choices=ROLES, default="employee")
+    birth_date = models.DateField(null=True, blank=True)
+    salary = models.IntegerField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(null=True, blank=True)
+
+    objects = CustomUserManager()
 
     USERNAME_FIELD = "email"
-    objects = AdvancedUserManager()
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
